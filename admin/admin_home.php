3a9174467a9041eb8c67d75d6ba17993257d8c18
zhangkecyrus <kzhan013@uottawa.ca>
2016-09-13 22:19:15 -0400

    main page

--- admin/admin_home.php
+++ admin/admin_home.php
@@ -9,26 +9,50 @@ require "templates/admin_home_header.php";
 ?>
 
 <nav class="navbar navbar-default navbar-fixed-top">
-  <div class="container-fluid">
-      <img src="assets/img/logo.png" alt="Avatar"/> 
-  </div>
+ 	<div class="container-fluid">
+    	<img src="assets/img/logo.png" alt="Avatar"/> 
+  	</div>
 </nav>
-<div class="container">
-	<h1>Page Content</h1>
-	<h1>Page Content</h1>
-	<h1>Page Content</h1>
-	<h1>Page Content</h1>
-	<h1>Page Content</h1>
-	<h1>Page Content</h1>
-	<h1>Page Content</h1>
-	<h1>Page Content</h1>
-	<h1>Page Content</h1>
-	<h1>Page Content</h1>
-	<h1>Page Content</h1>
-	<h1>Page Content</h1>
-	<h1>Page Content</h1>
-	<h1>Page Content</h1>
-	<h1>Page Content</h1>
+<div class="container main-body">
+	<div class="row">
+    	<div class="col-md-8 col-sm-8 col-xs-8">
+    		<div class="alert">
+    			
+    		</div>
+    	</div>
+    	<div class="col-md-4 col-sm-4 col-xs-4">
+    		<div class="alert">
+    			
+    		</div>
+    	</div>
+  	</div>
+  	<div class="row">
+    	<div class="col-md-3 col-sm-6 col-xs-12">
+    		<div class="main-component" >
+    			<img src="assets/img/linechart.png" alt="Avatar"/>
+    		</div>
+    	</div>
+    	<div class="col-md-3 col-sm-6 col-xs-12">
+    		<div class="main-component" >
+    			<img src="assets/img/gallery.png" alt="Avatar"/>
+    		</div>
+    	</div>
+    	<div class="col-md-3 col-sm-6 col-xs-12">
+    		<div class="main-component" >
+    			<img src="assets/img/note.png" alt="Avatar"/>
+    		</div>
+    	</div>
+    	<div class="col-md-3 col-sm-6 col-xs-12">
+    		<div class="main-component" >
+    			<img src="assets/img/web.png" alt="Avatar"/>
+    		</div>
+    	</div>
+    	<div class="col-md-3 col-sm-6 col-xs-12">
+    		<div class="main-component" >
+    			<img src="assets/img/password.png" alt="Avatar"/>
+    		</div>
+    	</div>
+  	</div>
 </div>
 <!-- CONTENT-WRAPPER SECTION END-->
 <?php require "templates/admin_home_footer.php"; ?>
