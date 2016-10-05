94bcdf8a9d8dbdd7be651c4965baa4f56fbb8a5d
zhangkecyrus <kzhan013@uottawa.ca>
2016-10-04 22:46:17 -0400

    admin_home GUI change

--- admin/admin_home.php
+++ admin/admin_home.php
@@ -16,19 +16,18 @@ $webLib = "web_library";
 
 switch($option_name) {
 	case 'photo':			
-					$include_flag           =   true;
                     $include_page			=   "photo_wall_edit.php";		
 					$page_title				=	"Photo Wall";			
 					break;
 	case 'web':			
-					$include_flag           =   true;
                     $include_page			=	"web_library_edit.php";		
-					$page_title				=	"Photo Wall";	
+					$page_title				=	"Web Lib";	
                     $web_types              =   $mainClassObj->getSchemaInfo($webTypes, "*", "", "", "", "", "");
                     $recently_added         =   $mainClassObj->getSchemaInfo($webLib, "*", "", "uri", "created_at", "DESC", "4");
 					break;	
 	default:				
-                    $include_flag           =   false;	
+                    $include_page           =   "admin_home_dashboard.php";     
+                    $page_title             =   "Dashboard";	
 					break;
 }
 
@@ -42,73 +41,69 @@ switch($option_name) {
 </nav>
 <div class="container">
 	<div class="row">
-    	<div class="col-md-8 col-sm-8 col-xs-8">
-    		<div class="news wrapper">
-    			<div class="ribbon-wrapper-green">
-    				<div class="ribbon-green" id="test">NEWS</div>
-    			</div>
-			<p>This year’s Adult Christmas Party will be held on the evening of Saturday, December 3rd at Algonquin College (same venue as last year).  For the past 5 years, the Christmas Party format has been a dinner followed by a comedy show.</p>
-    		</div>
-    	</div>
-    	<div class="col-md-4 col-sm-4 col-xs-4">
-    		<div class="news">
-    			<div> color change test </div>
-    		</div>
-    	</div>
-  	</div>
-    <div class="row">
-        <div class="alert alert-info col-md-8 col-sm-8 col-xs-8" id="main_component_guide">
-            <span class="glyphicon glyphicon-pushpin" aria-hidden="true"></span>
-            <span class="sr-only">Hint:</span>
-                &nbsp;Double click on module below
+        <div class="col-md-2 col-sm-6 col-xs-6">
+            <div class="main-component" style="background-color: crimson">
+                <a href="admin_finance.php">
+                    <img src="assets/img/linechart.png" alt="Finance"/>
+                </a>
+                <hr/>
+                <p class="main-component-description">FINANCE</p>
+            </div>
         </div>
-    </div>
-  	<div class="row">
-    	<div class="col-md-3 col-sm-6 col-xs-12">
-    		<div class="main-component" style="background-color: crimson">
-    			<a href="admin_finance.php">
-    				<img src="assets/img/linechart.png" alt="Finance"/>
-    			</a>
-    		</div>
-    	</div>
-    	<div class="col-md-3 col-sm-6 col-xs-12">
-    		<div class="main-component" style="background-color: dodgerblue;">
-    			<a  id="to_photo_wall_edit" href="admin_home.php?option=photo">
-    				<img src="assets/img/gallery.png" alt="Photo"/>
-    			</a>
-    		</div>
-    	</div>
-    	<div class="col-md-3 col-sm-6 col-xs-12">
-    		<div class="main-component" style="background-color: coral;">
-    			<a href="admin_home.php?option=note">
-    				<img src="assets/img/note.png" alt="Notes"/>
-    			</a>
-    		</div>
-    	</div>
-    	<div class="col-md-3 col-sm-6 col-xs-12">
-    		<div class="main-component" style="background-color: lightslategrey;">
-    			<a id="to_webform" href="admin_home.php?option=web">
-    				<img src="assets/img/web.png" alt="Userful Links"/>
-    			</a>	
-    		</div>
-    	</div>
-    	<div class="col-md-3 col-sm-6 col-xs-12">
-    		<div class="main-component" style="background-color: mediumseagreen;">
-    			<a href="admin_home.php?option=password">
-    				<img src="assets/img/password.png" alt="Keychain"/>
-    			</a>
-    		</div>
+        <div class="col-md-2 col-sm-6 col-xs-6">
+            <div class="main-component" style="background-color: dodgerblue;">
+                <a  id="to_photo_wall_edit" href="admin_home.php?option=photo">
+                    <img src="assets/img/gallery.png" alt="Photo"/>
+                </a>
+                <hr/>
+                <p class="main-component-description">PHOTO WALL</p>
+            </div>
+        </div>
+        <div class="col-md-2 col-sm-6 col-xs-6">
+            <div class="main-component" style="background-color: coral;">
+                <a href="admin_home.php?option=note">
+                    <img src="assets/img/note.png" alt="Notes"/>
+                </a>
+                <hr/>
+                <p class="main-component-description">NOTE</p>
+            </div>
+        </div>
+        <div class="col-md-2 col-sm-6 col-xs-6">
+            <div class="main-component" style="background-color: lightslategrey;">
+                <a id="to_webform" href="admin_home.php?option=web">
+                    <img src="assets/img/web.png" alt="Userful Links"/>
+                </a>
+                <hr/>
+                <p class="main-component-description">URI LIBRARY</p>    
+            </div>
+        </div>
+        <div class="col-md-2 col-sm-6 col-xs-6">
+            <div class="main-component" style="background-color: mediumseagreen;">
+                <a href="admin_home.php?option=password">
+                    <img src="assets/img/password.png" alt="Keychain"/>
+                </a>
+                <hr/>
+                <p class="main-component-description">KEYCHAIN</p>
+            </div>
+        </div>
+    	<div class="col-md-2 col-sm-6 col-xs-6">
+            <div class="news wrapper">
+                <div class="ribbon-wrapper-green">
+                    <div class="ribbon-green" id="test">NEWS</div>
+                </div>
+            <p>This year</p>
+            </div>
     	</div>
   	</div>
-  	<?php
-        if($include_flag) {
-             require "partials/".$include_page;
-        } 
-     ?>
-    <div class="analyze-result row">
-        <iframe src="" class="col-xs-12 analyze-result-window"></iframe>
-        <span class="glyphicon glyphicon-chevron-down analyze-close" aria-hidden="true" style="position: absolute; right: 50px; top: 20px; font-size: 30px; cursor: pointer;"></span>
-    </div>
+</div>
+
+<?php
+    require "partials/".$include_page;
+?>
+
+<div class="analyze-result row">
+    <iframe src="" class="col-xs-12 analyze-result-window"></iframe>
+    <span class="glyphicon glyphicon-chevron-down analyze-close" aria-hidden="true" style="position: absoluright: 50px; top: 20px; font-size: 30px; cursor: pointer;"></span>
 </div>
 <!-- CONTENT-WRAPPER SECTION END-->
 <?php require "templates/admin_home_footer.php"; ?>
---
+++ admin/partials/admin_home_dashboard.php
@@ -0,0 +1,11 @@
+<div class="container" style="margin-top:2em;">
+	<div class="row">
+		<div class="col-md-8 col-sm-6 col-xs-6" style="min-height:400px;">
+            <div style="background-color:#efefef;min-height:400px;margin-bottom:3em;"></div>
+        </div>
+        
+        <div class="col-md-4 col-sm-6 col-xs-6" style="min-height:400px;">
+        	<div style="background-color:#efefef;min-height:400px;margin-bottom:3em;"></div>
+        </div>
+	</div>
+</div>
