a46f21ad73029014b764235e0a0d202234135c8f
zhangkecyrus <kzhan013@uottawa.ca>
2016-10-04 22:48:53 -0400

    make up

--- admin/partials/admin_home_dashboard.php
+++ admin/partials/admin_home_dashboard.php
@@ -1,10 +1,10 @@
 <div class="container" style="margin-top:2em;">
 	<div class="row">
-		<div class="col-md-8 col-sm-6 col-xs-6" style="min-height:400px;">
+		<div class="col-md-8 col-sm-6 col-xs-6">
             <div style="background-color:#efefef;min-height:400px;margin-bottom:3em;"></div>
         </div>
         
-        <div class="col-md-4 col-sm-6 col-xs-6" style="min-height:400px;">
+        <div class="col-md-4 col-sm-6 col-xs-6">
         	<div style="background-color:#efefef;min-height:400px;margin-bottom:3em;"></div>
         </div>
 	</div>
