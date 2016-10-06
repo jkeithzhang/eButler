7abd9ad9560fea497fed74d9512abc874756ca8f
zhangkecyrus <kzhan013@uottawa.ca>
2016-10-05 21:47:00 -0400

    dashboard GUI update

--- admin/partials/admin_home_dashboard.php
+++ admin/partials/admin_home_dashboard.php
@@ -1,11 +1,20 @@
 <div class="container" style="margin-top:2em;">
 	<div class="row">
-		<div class="col-md-8 col-sm-6 col-xs-6">
-            <div style="background-color:#efefef;min-height:400px;margin-bottom:3em;"></div>
+        <div class="col-md-4 col-sm-12 col-xs-12" style="background-color:#efefef;margin-bottom:3em;">
+        	<div class="row">
+        		<div class="col-md-5 col-sm-6 col-xs-6" id="dashboard-user-name">
+					<div class="glyphicon glyphicon-user" aria-hidden="true"></div>
+					<hr/>
+					<p>Ke Zhang</p>
+        		</div>
+        		<div class="col-md-7 col-sm-6 col-xs-6" style="border-left:2px #fff; min-height:150px;margin-top:1em;margin-bottom:1em;"></div>
+        	</div>
         </div>
-        
-        <div class="col-md-4 col-sm-6 col-xs-6">
-        	<div style="background-color:#efefef;min-height:400px;margin-bottom:3em;"></div>
+
+		<div class="col-md-8 col-sm-12 col-xs-12">
+            <div style="background-color:#efefef;min-height:400px;margin-bottom:3em;">
+            	
+            </div>
         </div>
 	</div>
 </div>
